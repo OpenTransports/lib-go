72588723306293077c921a22c246c8e22d46d3b6
Louis Chemineau <artonge.c@gmail.com>
2017-08-18 18:32:30 +0300

    Add omitempty on optional fields

--- models/Transport.go
+++ models/Transport.go
@@ -36,29 +36,29 @@ type (
 	TransportType struct {
 		ID   int    `json:"id"`
 		Name string `json:"name"`
-		Icon string `json:"icon"`
+		Icon string `json:"icon,omitempty"`
 	}
 
 	// Information - structure for transports informations (passage, nb available)
 	Information struct {
-		Title     string   `json:"title"`     // Direction of the passage
-		Content   []string `json:"content"`   // Time, is array of string to support non numeric values
-		Warn      bool     `json:"warn"`      // Display the content differently to warn the user
-		Timestamp int      `json:"timestamp"` // The time at wich the content was updated
+		Title     string   `json:"title"`               // Direction of the passage
+		Content   []string `json:"content"`             // Time, is array of string to support non numeric values
+		Warn      bool     `json:"warn,omitempty"`      // Display the content differently to warn the user
+		Timestamp int      `json:"timestamp,omitempty"` // The time at wich the content was updated
 	}
 
 	// Transport - Can be embedded by custom Transports structs
 	// Gives some usefull properties and two methods
 	// Each Transports struct still need to implement UpdateInfo
 	Transport struct {
-		ID           string        `json:"id"`           // ID of the Transport, should be specific to the Agency
-		AgencyID     string        `json:"agencyID"`     // ID of the associated agency
-		Type         int           `json:"type"`         // String identifing the kind of transport
-		Name         string        `json:"name"`         // The name of the transport, doesn't have to be unique
-		Line         string        `json:"line"`         // The line of the transport
-		Position     Position      `json:"position"`     // Position of the transport
-		Informations []Information `json:"informations"` // Informations on the transports
-		IconURL      string        `json:"iconURL"`      // URL to the icon
+		ID           string        `json:"id"`             // ID of the Transport, should be specific to the Agency
+		AgencyID     string        `json:"agencyID"`       // ID of the associated agency
+		Type         int           `json:"type"`           // String identifing the kind of transport
+		Name         string        `json:"name"`           // The name of the transport, doesn't have to be unique
+		Line         string        `json:"line,omitempty"` // The line of the transport
+		Position     Position      `json:"position"`       // Position of the transport
+		Informations []Information `json:"informations"`   // Informations on the transports
+		IconURL      string        `json:"iconURL"`        // URL to the icon
 	}
 )
 
